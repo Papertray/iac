11751463537794245a545c11f3d714d19d14121b
Papertray <adminboris@gmail.com>
2020-05-23 17:01:28 +0200

    Table annotations added, Base64 Encoding and decoding added. ImageProvider functionality added. File model added

--- src/main/java/com/iac/webshop/helpers/ImageProvider.java
+++ src/main/java/com/iac/webshop/helpers/ImageProvider.java
@@ -20,13 +20,6 @@ public class ImageProvider {
     public static String getPathString(String folderName, String fileName) {
         return (rootFolder + folderName + "/" + fileName);
     }
-//    public static void CreateImage(String folder, String id) {
-//        BufferedImage bImage = null;
-//        try {
-//            ImageIO.write(bImage, "jpg", new File(rootFolder + folder + "/" + id + ".png"));
-//        } catch (IOException e) {
-//        }
-//    }
 
     public static void saveImage(String fileData, String folderName, String fileName) throws IOException {
         Files.createDirectories(Paths.get(rootFolder + folderName + "/"));
@@ -40,46 +33,4 @@ public class ImageProvider {
     public static void deleteImage(String folderName, String fileName) throws IOException {
         Files.delete(getFullPath(folderName, fileName));
     }
-//
-//    public static BufferedImage ReadImage(String folder, String id) {
-//        try
-//        {
-//            return ImageIO.read(new File(rootFolder + folder + "/" + id + ".png"));
-//        }
-//        catch (IOException e)
-//        {
-//            return null;
-//        }
-//    }
-//
-//    public static String encoder(String imagePath) {
-//        File file = new File(imagePath);
-//        try (FileInputStream imageInFile = new FileInputStream(file)) {
-//            // Reading a Image file from file system
-//            String base64Image = "";
-//            byte imageData[] = new byte[(int) file.length()];
-//            imageInFile.read(imageData);
-//            base64Image = Base64.getEncoder().encodeToString(imageData);
-//            return base64Image;
-//        } catch (FileNotFoundException e) {
-//            System.out.println("Image not found " + e);
-//        } catch (IOException ioe) {
-//            System.out.println("Exception while reading the Image " + ioe);
-//        }
-//        return null;
-//    }
-//
-//    public static void decoder(String base64Image, String folder) {
-//        // File.(rootFolder+folder);
-//        try (FileOutputStream imageOutFile = new FileOutputStream(rootFolder+folder)) {
-//            // Converting a Base64 String into Image byte array
-//            byte[] imageByteArray = Base64.getDecoder().decode(base64Image);
-//            File file = new File(rootFolder+folder);
-//            imageOutFile.write(imageByteArray);
-//        } catch (FileNotFoundException e) {
-//            System.out.println("Image not found " + e);
-//        } catch (IOException ioe) {
-//            System.out.println("Exception while reading the Image " + ioe);
-//        }
-//    }
 }
