6db266f5b8fbcea670b69de73b963b6ecceb5967
ijanszen <idrisjanszen@gmail.com>
2020-05-23 12:58:53 +0200

    smoll change to test git for deva

--- src/main/java/com/iac/webshop/controllers/AccountController.java
+++ src/main/java/com/iac/webshop/controllers/AccountController.java
@@ -1,6 +1,5 @@
 package com.iac.webshop.controllers;
 
-import com.iac.webshop.exceptions.AccountNotFoundException;
 import com.iac.webshop.models.Account;
 import com.iac.webshop.services.interfaces.IAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AccountController {
+
+    //test for pushing
 
     @Autowired
     IAccountService accountService;
