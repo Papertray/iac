7a487cb9929f6ead2dd0648ab18497ed12ac964c
Boris Blom <adminboris@gmail.com>
2020-07-12 16:57:52 +0200

    Simplified DTO usage with generic helper function

---
+++ src/main/java/com/iac/webshop/dto/AccountDTO.java
@@ -0,0 +1,12 @@
+package com.iac.webshop.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class AccountDTO {
+    long id;
+    String email;
+}
